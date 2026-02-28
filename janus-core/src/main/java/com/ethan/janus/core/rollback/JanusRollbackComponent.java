f627e80f9bec5734822638740981edcc934dac77
别小楼 <45735033+sxd19911008@users.noreply.github.com>
2026-02-28 16:00:16 +0800

    fix: 解决没有上层事务时，resetRollbackOnly太迟导致框架内提交事务报错

--- janus-core/src/main/java/com/ethan/janus/core/rollback/JanusRollbackComponent.java
+++ janus-core/src/main/java/com/ethan/janus/core/rollback/JanusRollbackComponent.java
@@ -2,7 +2,6 @@ package com.ethan.janus.core.rollback;
 
 import com.ethan.janus.core.exception.JanusException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.jdbc.datasource.ConnectionHolder;
 import org.springframework.jdbc.datasource.DataSourceUtils;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,13 +19,48 @@ public class JanusRollbackComponent implements JanusRollback {
     private DataSource dataSource;
     @Autowired(required = false)
     private JanusRollbackClearCache janusRollbackClearCache;
-    @Lazy
-    @Autowired
-    private JanusRollbackComponent thisBean;
 
+    @Transactional(rollbackFor = Throwable.class)
     public void branchRollback(Runnable runnable) {
         try {
-            thisBean.transactional(runnable);
+            if (dataSource == null) {
+                runnable.run();
+                return;
+            }
+            Connection connection = DataSourceUtils.getConnection(dataSource);
+            String uuid = UUID.randomUUID().toString();
+            // 事务保存点
+            Savepoint savepoint;
+            try {
+                savepoint = connection.setSavepoint(uuid);
+            } catch (SQLException e) {
+                throw new JanusException("savepoint 异常", e);
+            }
+            try {
+                // 执行分支方法
+                runnable.run();
+            } finally {
+                try {
+                    // 回滚到事务保存点，而不是回滚整个事务。
+                    connection.rollback(savepoint);
+                } catch (SQLException e) {
+                    //noinspection ThrowFromFinallyBlock
+                    throw new JanusException("savepoint回滚报错", e);
+                }
+
+                /*
+                 * 【注意】如果使用了类似 MyBatis 的框架，则执行完比对分支，要清理一级缓存，防止因为缓存导致查询错误。
+                 * 因为`connection.rollback(savepoint);`不会清理一级缓存。导致事务结束时，无法清理一级缓存。
+                 * 必须在事务结束之前，清理一级缓存！否则缓存清理不掉。一级缓存绑定事务。
+                 * 如果是报错导致事务回滚，则会自动清理一级缓存，没有此问题。
+                 * 以 MyBatis 的清理一级缓存做示例：  sqlSessionTemplate.clearCache();
+                 */
+                if (janusRollbackClearCache == null) {
+                    //noinspection ThrowFromFinallyBlock
+                    throw new JanusException("请提供 JanusRollbackClearCache 实现");
+                }
+                janusRollbackClearCache.clearCache();
+            }
         } finally {
             /*
              * 【重要】必须自己开的事务结束后再 resetRollbackOnly
@@ -37,48 +71,6 @@ public class JanusRollbackComponent implements JanusRollback {
                 // 【重要】比对分支报错，主分支不可回滚，所以要重置 rollbackOnly 为 false
                 holder.resetRollbackOnly();
             }
-        }
-    }
-
-    @Transactional(rollbackFor = Throwable.class)
-    public void transactional(Runnable runnable) {
-        if (dataSource == null) {
-            runnable.run();
-            return;
-        }
-        Connection connection = DataSourceUtils.getConnection(dataSource);
-        String uuid = UUID.randomUUID().toString();
-        // 事务保存点
-        Savepoint savepoint;
-        try {
-            savepoint = connection.setSavepoint(uuid);
-        } catch (SQLException e) {
-            throw new JanusException("savepoint 异常", e);
-        }
-        try {
-            // 执行分支方法
-            runnable.run();
-        } finally {
-            try {
-                // 回滚到事务保存点，而不是回滚整个事务。
-                connection.rollback(savepoint);
-            } catch (SQLException e) {
-                //noinspection ThrowFromFinallyBlock
-                throw new JanusException("savepoint回滚报错", e);
-            }
-
-            /*
-             * 【注意】如果使用了类似 MyBatis 的框架，则执行完比对分支，要清理一级缓存，防止因为缓存导致查询错误。
-             * 因为`connection.rollback(savepoint);`不会清理一级缓存。导致事务结束时，无法清理一级缓存。
-             * 必须在事务结束之前，清理一级缓存！否则缓存清理不掉。一级缓存绑定事务。
-             * 如果是报错导致事务回滚，则会自动清理一级缓存，没有此问题。
-             * 以 MyBatis 的清理一级缓存做示例：  sqlSessionTemplate.clearCache();
-             */
-            if (janusRollbackClearCache == null) {
-                //noinspection ThrowFromFinallyBlock
-                throw new JanusException("请提供 JanusRollbackClearCache 实现");
-            }
-            janusRollbackClearCache.clearCache();
         }
     }
 }
